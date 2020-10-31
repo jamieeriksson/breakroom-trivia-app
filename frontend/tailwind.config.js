5cf58078089b8dbcec726cb83ec94dbf9e61f2b4
Jamie Eriksson <jamieeriksson@gmail.com>
2020-10-30 20:59:24 -0700

    Set up webpack, tailwind, and postcss config files

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    colors: {
+      red: "#CF5C36",
+      blue: {
+        light: "#657ED4",
+        dark: "#3626A7",
+      },
+      green: "#26AC1B",
+      black: "#0D0106",
+      white: "#FBFBFF",
+      transparent: "transparent",
+      gray: {
+        light: "#EBEBFD",
+        54: "#545454",
+        100: "#f7fafc",
+        200: "#edf2f7",
+        300: "#e2e8f0",
+        400: "#cbd5e0",
+        500: "#a0aec0",
+        600: "#718096",
+        700: "#4a5568",
+        800: "#2d3748",
+        900: "#1a202c",
+      },
+    },
+    fontFamily: {
+      sans: ["Rubik", "sans-serif"],
+      cursive: ["Architects Daughter", "cursive"],
+      capital: ["Bebas Neue", "cursive"],
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Lato", "sans-serif"],
+        title: ["Fjalla One", "sans-serif"],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,38 +1,67 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { HotModuleReplacementPlugin } = require("webpack");
+const { argv } = require("process");
 
-module.exports = {
-  mode: "development",
-  entry: path.resolve(__dirname, "src", "index.js"),
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
+module.exports = (env, argv) => {
+  return {
+    mode: "development",
+    entry: path.resolve(__dirname, "src", "index.js"),
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          include: path.resolve(__dirname, "src"),
+          exclude: /node_modules/,
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
         },
-      },
+        {
+          test: /\.css$/i,
+          include: path.resolve(__dirname, "src"),
+          exclude: /node_modules/,
+          use: [
+            "style-loader",
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hmr: argv.mode === "development",
+              },
+            },
+            {
+              loader: "css-loader",
+              options: {
+                importLoaders: 1,
+              },
+            },
+            "postcss-loader",
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html"),
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].bundle.css",
+        chunkFilename: "[id].css",
+      }),
+      new HotModuleReplacementPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html"),
-    }),
-    new CleanWebpackPlugin({
-      // Simulate the removal of files
-      dry: true,
-      // Write Logs to Console
-      verbose: true,
-      // Automatically remove all unused webpack assets on rebuild
-      cleanStaleWebpackAssets: true,
-      protectWebpackAssets: false,
-    }),
-  ],
+    devServer: {
+      open: true,
+      clientLogLevel: "silent",
+      contentBase: "./dist",
+      historyApiFallback: true,
+      hot: true,
+    },
+  };
 };
