826d442f2c85308983e708d17683ccafd6aec6f8
Jamie Eriksson <jamieeriksson@gmail.com>
2020-11-01 11:51:42 -0800

    Add loading and error style on start and quiz view

--- frontend/src/components/trivia-quiz.js
+++ frontend/src/components/trivia-quiz.js
@@ -310,9 +310,27 @@ class TriviaQuiz extends React.Component {
     } = this.state;
 
     if (error) {
-      return <div>Error: {error.message}</div>;
+      return (
+        <div className="bg-blue-light w-screen h-screen flex justify-center place-items-center">
+          <Header fromQuizView={false} />
+          <QuestionBg className="z-0" />
+          <div className="z-10 max-w-6xl w-full py-10 px-6 bg-gray-light rounded-xl flex flex-col place-items-center">
+            <p className="text-3xl text-center">
+              An error occured: {error.message}
+            </p>
+          </div>
+        </div>
+      );
     } else if (!isLoaded) {
-      return <div>Loading...</div>;
+      return (
+        <div className="bg-blue-light w-screen h-screen flex justify-center place-items-center">
+          <Header fromQuizView={false} />
+          <QuestionBg className="z-0" />
+          <div className="z-10 max-w-6xl w-full py-10 px-6 bg-gray-light rounded-xl flex flex-col place-items-center">
+            <p className="text-3xl text-center">Loading...</p>
+          </div>
+        </div>
+      );
     } else {
       return (
         <div className="bg-blue-light w-screen h-screen flex justify-center place-items-center">
